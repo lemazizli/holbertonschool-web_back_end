ba6fbd63b0ab96204c95d7f8b2b7f2866a0e5e5a
Lem <lemazizli7@gmail.com>
2026-05-09 02:47:44 +0400

    done
    Refactor log_stats function to run directly in main block.

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
-"""
-Module for providing stats about Nginx logs stored in MongoDB.
-"""
+"""Provide stats about Nginx logs stored in MongoDB."""
 from pymongo import MongoClient
 
 
-def log_stats():
-    """
-    Displays stats about Nginx logs in the collection 'nginx' of database 'logs'.
-    """
+if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
@@ -21,12 +16,7 @@ def log_stats():
         count = nginx_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = nginx_collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
     print(f"{status_check} status check")
-
-
-if __name__ == "__main__":
-    log_stats()
